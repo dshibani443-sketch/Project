70c68e1d34086768ccb7354449027ebe7f6e696c
suman <dshibani443@gmail.com>
2026-03-31 08:43:35 +0530

    versal fix config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(),tailwindcss()],
+  base:"/",
+  build:{
+    outDir:"dist"//vercal use this folder
+  }
 })
